cb087176c4627931a24c6ab498bbd2248b522811
Ray Ashir <rayeed.ashir@gmail.com>
2021-09-19 09:36:50 -0700

    added delete

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,9 @@
  const router = require('express').Router();
- const { Post } = require('../../models');
+ const { Post, User } = require('../../models');
+ const withAuth = require('../../utils/auth');
 
- router.post('/', async (req, res) => {
+
+ router.post('/', withAuth, async (req, res) => {
    try {
      const userData = await Post.create({
       ...req.body,
@@ -19,6 +21,48 @@
      res.status(400).json(err);
    }
  });
+
+ router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['user_name'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+ router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // router.post('/login', async (req, res) => {
 //   try {
