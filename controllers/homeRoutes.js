d9d9126dba97aa86dda275a5bbb1c7a0682240b9
Ray Ashir <rayeed.ashir@gmail.com>
2021-09-19 06:59:14 -0700

    fixed dashboard to show username and update routes to use dashboard instead of profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -52,17 +52,19 @@ router.get('/project/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ['user_password'] },
       include: [{ model: Post }],
     });
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    console.log(user);
+
+    res.render('dashboard', {
       ...user,
       logged_in: true
     });
@@ -74,7 +76,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
 
