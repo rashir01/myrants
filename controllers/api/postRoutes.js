7307f3289486c36a12dd7ad40c1973b9fb0bafae
Ray Ashir <rayeed.ashir@gmail.com>
2021-09-19 07:15:24 -0700

    added ability to submit a post and have it show up in the landing page

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,19 +1,23 @@
  const router = require('express').Router();
- const { User } = require('../../models');
+ const { Post } = require('../../models');
 
  router.post('/', async (req, res) => {
-//   try {
-//     const userData = await User.create(req.body);
+   try {
+     const userData = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
 
-//     req.session.save(() => {
-//       req.session.user_id = userData.id;
-//       req.session.logged_in = true;
+     
+     req.session.save(() => {
+       req.session.user_id = userData.id;
+       req.session.logged_in = true;
 
-//       res.status(200).json(userData);
-//     });
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
+       res.status(200).json(userData);
+     });
+   } catch (err) {
+     res.status(400).json(err);
+   }
  });
 
 // router.post('/login', async (req, res) => {
@@ -58,4 +62,4 @@
 //   }
 // });
 
-// module.exports = router;
+ module.exports = router;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,21 +1,20 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const title = document.querySelector('#project-name').value.trim();
+  const content = document.querySelector('#project-desc').value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
+  if (title && content) {
+    const response = await fetch(`/api/posts`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ title, content }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/dashboard');
     } else {
       alert('Failed to create project');
     }
@@ -26,12 +25,12 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/posts/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/dashboard');
     } else {
       alert('Failed to delete project');
     }
