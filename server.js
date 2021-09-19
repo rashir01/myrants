51f0fb2db6f6f34f2a05824c388b432f60e91689
Ray Ashir <rayeed.ashir@gmail.com>
2021-09-18 18:38:11 -0700

    added dashboard routing to nav link

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ sequelize.sync({ force: false }).then(() => {
 
 /* IN PROGRESS */ 
 /*
-WHEN I visit the site for the first time
-THEN I am presented with the homepage, which includes existing blog posts if any have been posted; navigation links for the homepage and the dashboard; and the option to log in
+WHEN I click on any other links in the navigation
+THEN I am prompted to either sign up or sign in
 */
 
 
@@ -51,6 +51,9 @@ THEN I am presented with the homepage, which includes existing blog posts if any
 /*
 WHEN I click on the homepage option
 THEN I am taken to the homepage
+
+WHEN I visit the site for the first time
+THEN I am presented with the homepage, which includes existing blog posts if any have been posted; navigation links for the homepage and the dashboard; and the option to log in
 
 */
 /* TODO */
@@ -70,8 +73,7 @@ GIVEN a CMS-style blog site
 
 
 
-WHEN I click on any other links in the navigation
-THEN I am prompted to either sign up or sign in
+
 
 WHEN I choose to sign up
 THEN I am prompted to create a username and password
