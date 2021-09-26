a183690e8ccba3fae8a11b8880bd102178221d64
Ray Ashir <rayeed.ashir@gmail.com>
2021-09-26 09:33:34 -0700

    show update fields only after update button click

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { Comment } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const userData = await Comment.create(req.body);
 
     req.session.save(() => {
       req.session.user_id = userData.id;
@@ -16,46 +16,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
 
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
 
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
 
 module.exports = router;
--- public/js/update.js
+++ public/js/update.js
@@ -1,34 +1,72 @@
 const updateFormHandler = async (event) => {
   event.preventDefault();
-  console.log("xxxxxxxxxxxthis is new form handler!!!!!!!!!");
-  //alert('This is update!')
-  
+  //initializations
+  var titleDiv = document.getElementById("title-div");
+  var bodyDiv = document.getElementById("body-div");
+  let title = "";
+  let content = "";
 
-  const title = document.querySelector('#post-title').value.trim();
-  const content = document.querySelector('#post-content').value.trim();
-  const postID =  document.getElementById('post-id').textContent;
+  //assign values to title and content if they exist
+  if (titleDiv.hasChildNodes() && bodyDiv.hasChildNodes()) {
+    title = document.getElementById("post-title").value.trim();
+    content = document.getElementById('post-content').value.trim();
+  }
 
+  const postID = document.getElementById('post-id').textContent;
 
-  //alert(`title ${title} and content ${content} id ${postID}`);
-  // console.log(event);
+  //if the user provided a title and content, then update
+  if (title && content) {
+    const response = await fetch(`/api/posts/${postID}`, {
+      method: 'PUT',
+      body: JSON.stringify({ title, content }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-    if (title && content) {
-      const response = await fetch(`/api/posts/${postID}`, {
-        method: 'PUT',
-        body: JSON.stringify({ title, content }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+    if (response.ok) {
+      console.log("response stringify " + JSON.stringify(response))
+      document.location.replace('/dashboard');
 
-      if (response.ok) {
-        console.log("response stringify " + JSON.stringify(response))
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to create project');
-      }
+    } else {
+      alert('Failed to create project');
     }
+  } else {
+    //if no title and content, then show the labels and fileds
+    removeAllChildNodes(titleDiv);
+    removeAllChildNodes(bodyDiv);
+
+    alert('unable to get title and content');
+    var updateTitleLable = document.createElement("label");
+    updateTitleLable.setAttribute('for', "project-name");
+    updateTitleLable.textContent = "Post Title:";
+    var updateTitleTextBox = document.createElement("input");
+    updateTitleTextBox.setAttribute('class', "form-input");
+    updateTitleTextBox.setAttribute('type', 'text');
+    updateTitleTextBox.setAttribute('id', "post-title"); updateTitleTextBox.setAttribute('name', "project-name");
+    titleDiv.appendChild(updateTitleLable);
+    titleDiv.appendChild(updateTitleTextBox);
+
+    var updateBodyLabel = document.createElement("label");
+    updateBodyLabel.setAttribute('for', 'project-desc');
+    updateBodyLabel.textContent = "Post Content:";
+    var updateBodyTextBox = document.createElement("textarea");
+    updateBodyTextBox.setAttribute('class', 'form-input');
+    updateBodyTextBox.setAttribute('id', 'post-content');
+    updateBodyTextBox.setAttribute('name', 'project-desc');
+
+    bodyDiv.appendChild(updateBodyLabel);
+    bodyDiv.appendChild(updateBodyTextBox);
+
+  }
 };
+
+//clear children so that they dont keep getting added everytime user presses update
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
 
 document
   .querySelector('.update-button')
